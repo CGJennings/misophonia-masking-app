c4a84ee06ee963693919e8b935589a5caf5502f0
Chris Jennings <25697451+CGJennings@users.noreply.github.com>
2022-12-23 17:53:01 -0500

    move button images to inline css (#4)
    moves SVG code out of the script file

--- app/index.js
+++ app/index.js
@@ -205,25 +205,15 @@ for (let i=0; i<NOISE_TYPES.length; ++i) {
     audioControlEl.appendChild(btn);
 }
 
-// extra setup for the silence button
+// extra setup for the "Silence" and "Custom" buttons
 let blackBtn = audioControlEl.firstChild;
 blackBtn.classList.add("active");
 blackBtn.setAttribute("aria-label", "silence");
-blackBtn.innerHTML = `<svg viewBox="0 0 24 24">
-  <path fill="#fff" d="M3 9h4l5-5v16l-5-5H3V9m13.59 3L14 9.41L15.41 8L18 10.59L20.59 8L22
-                       9.41L19.41 12L22 14.59L20.59 16L18 13.41L15.41 16L14 14.59L16.59 12Z"/>
-</svg>`;
+blackBtn.innerText = "";
 
-// extra setup for the custom button
 blackBtn = audioControlEl.lastChild;
-blackBtn.innerHTML = `<svg viewBox="0 0 24 24">
-<path fill="#fff" d="M8 13C6.14 13 4.59 14.28 4.14 16H2V18H4.14C4.59 19.72 6.14 21 8 21S11.41
-                       19.72 11.86 18H22V16H11.86C11.41 14.28 9.86 13 8 13M8 19C6.9 19 6 18.1
-                       6 17C6 15.9 6.9 15 8 15S10 15.9 10 17C10 18.1 9.1 19 8 19M19.86 6C19.41
-                       4.28 17.86 3 16 3S12.59 4.28 12.14 6H2V8H12.14C12.59 9.72 14.14 11 16
-                       11S19.41 9.72 19.86 8H22V6H19.86M16 9C14.9 9 14 8.1 14 7C14 5.9 14.9
-                       5 16 5S18 5.9 18 7C18 8.1 17.1 9 16 9Z"/>
-</svg>`;
+blackBtn.setAttribute("aria-label", "custom noise");
+blackBtn.innerText = "";
 
 // create the equalizer controls
 const eqControls = document.querySelector("custom-eq");
