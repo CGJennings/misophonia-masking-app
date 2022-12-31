d09889bf5918778fc59c98b7c8077edfc08c8c39
Christopher Jennings <25697451+CGJennings@users.noreply.github.com>
2022-12-30 23:41:04 -0500

    control conversion quality to better reproduce noise

--- clip-sources/make-clips-from-sources.js
+++ clip-sources/make-clips-from-sources.js
@@ -5,33 +5,57 @@ const { exit } = require("process");
 
 const DIR_SOURCE_CLIPS = __dirname;
 
+function timestamp(path) {
+    return fs.existsSync(path) ? fs.statSync(path).mtimeMs : 0;
+}
+
 function outpath(wavfile, ext) {
     const name = path.parse(wavfile).name;
     return path.join(DIR_SOURCE_CLIPS, "..", "app", "clips", `${name}.${ext}`);
 }
 
-function ffmpeg(infile, outfile) {
+function ffmpeg(infile, outext) {
+    const outfile = outpath(infile, outext);
+    if (timestamp(infile) < timestamp(outfile)) {
+        console.log(`  skipping ${outext}, already up to date`);
+        return;
+    }
+
+    const extraArgs = {
+        flac: ["-af", "aformat=s16:44100"],
+        mp3: ["-c:a", "libmp3lame", "-q:a", "0", "-abr", "1"],
+        ogg: ["-c:a", "libopus"],
+    }[outext] || [];
+
+    const args = ["-v", "error", "-i", infile, ...extraArgs, "-map_metadata", "-1", "-y", outfile];
     try {
         let po = spawnSync(
-            "ffmpeg", ["-v", "error", "-i", infile, "-map_metadata", "-1", "-y", outfile],
+            "ffmpeg", args,
             {
                 stdio: ["ignore", "ignore", "pipe"]
             }
         );
-        if (po.stderr && po.stderr.byteLength > 0) {
-            console.log(po.stderr.toString());
-        }
         if (po.error) throw error;
+        if (po.stderr && po.stderr.byteLength > 0) {
+            throw {
+                status: 20,
+                message: "conversion error",
+                stderr: po.stderr
+            }
+        }
     } catch (error) {
         console.log(`${error.status} ${error.message}\n${error.stderr.toString()}`);
+        console.log(args);
         exit(1);
     }
+
+    console.log(`  wrote ${outext} format clip`);
 }
 
 function convert(wavfile) {
     console.log(`converting ${wavfile}...`);
-    ffmpeg(wavfile, outpath(wavfile, "ogg"));
-    ffmpeg(wavfile, outpath(wavfile, "mp3"));
+    ffmpeg(wavfile, "ogg");
+    ffmpeg(wavfile, "mp3");
 }
 
 fs.readdirSync(DIR_SOURCE_CLIPS)
